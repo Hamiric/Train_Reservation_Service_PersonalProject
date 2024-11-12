84e166f75b21207469666b08f358d6474f7301be
Hamiric <do9870@naver.com>
2024-11-12 20:54:27 +0900

    Feat : "StationListPage 레이아웃 제작"
    StationListPage 레이아웃 제작

--- lib/station_list_page/station_list_page.dart
+++ lib/station_list_page/station_list_page.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
 class StationListPage extends StatelessWidget {
-  const StationListPage({super.key});
+  StationListPage({super.key});
+
+  final List<String> stationName = [
+    '수서',
+    '동탄',
+    '평택지제',
+    '천안아산',
+    '오송',
+    '대전',
+    '김천구미',
+    '동대구',
+    '경주',
+    '울산',
+    '부산'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +23,38 @@ class StationListPage extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: Text('출발역'),
+      ),
+      body: ListView.builder(
+        itemCount: stationName.length,
+        itemBuilder: (BuildContext context, int index) {
+        return stationList(stationName[index]);
+      }),
+    );
+  }
+
+  Widget stationList(String station) {
+    return GestureDetector(
+      onTap: () {
+        // 
+      },
+      child: Container(
+        height: 50,
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey[300]!))
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              station,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
